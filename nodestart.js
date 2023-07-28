abf9a0383685e76f699597708de991b7b7d91cd9
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-28 12:18:48 -0300

    Update nodestart.js

--- nodestart.js
+++ nodestart.js
@@ -1,10 +1,3 @@
-const http = require('http');
+console.log("Márcio");
 
-http.createServer((requisicao, resposta) => {
-    resposta.writeHead(200, {
-        'Content-Type' : 'text/plain'
-    });
-    resposta.write('Marcio Server\n');
-    resposta.end();
-}).listen(1337);
 
